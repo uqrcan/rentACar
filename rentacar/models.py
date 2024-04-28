bd6fb5294de4d0edf8af3b512aebc46557b79e5e
Uğur <uqrcan.01@gmail.com>
2024-04-29 01:12:44 +0300

    models

--- rentacar/models.py
+++ rentacar/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
+from django.conf import settings
 
-# Create your models here.
+class Car(models.Model):
+    plate_number = models.CharField(max_length=100, validators=[MaxLengthValidator(17)])  
+    brand = models.CharField(max_length=50)  
+    model = models.CharField(max_length=50)  
+    year = models.PositiveSmallIntegerField()  
+    gear = models.CharField(max_length=50)  
+    rent_per_day = models.IntegerField()  
+    availability = models.BooleanField(default=True)  
+
+    def _str_(self):
+        return f"{self.brand} {self.model} - {self.plate_number}"
+
+class Reservation(models.Model):
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)  
+    start_date = models.DateField()  
+    end_date = models.DateField() 
+
+    def _str_(self):
+        return f"Reservation for {self.car} by {self.customer} from {self.start_date} to {self.end_date}"
+
+    class Meta:
+        unique_together = ('customer', 'car', 'start_date', 'end_date')  # Eşsiz rezervasyon kombinasyonları için
